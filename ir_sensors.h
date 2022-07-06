baa347147012884732e307ef84f74f1e56bae607
anqi <74876670+anqixxx@users.noreply.github.com>
2022-07-06 14:48:26 -0700

    Update ir_sensors.h

--- ir_sensors.h
+++ ir_sensors.h
@@ -4,5 +4,16 @@
 #include <Arduino.h>
 
 //Enter in function definitions for ir sensing, with the code located in ir_sensors.cpp
+void readIR();
+
+void setupFrequencyReference();
+
+void displayArray(double vals[]);
+
+bool takeAnalogReadings(uint16_t* p_numReadings = nullptr, double** p_analogVals = nullptr, byte pin = PA7);
+
+double correlateTo1kz(double[]);
+
+double takeSignalSample(double** p_analogVals = nullptr, byte pin = PA7);
 
 #endif
