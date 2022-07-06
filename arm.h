cbcd3a65dafe44b3b769fd02033ece34e35371b4
anqi <74876670+anqixxx@users.noreply.github.com>
2022-07-06 14:27:17 -0700

    Create arm.h

---
+++ arm.h
@@ -0,0 +1,9 @@
+// Arm Functions, height and angle for the claw
+#ifndef ARM_H
+#define ARM_H
+
+#include <Arduino.h>
+
+//Enter in function definitions for arm functions, with the code located in arm.cpp
+
+#endif
