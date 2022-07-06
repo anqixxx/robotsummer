3fd372700ac0ad97b8ff050ccbac4e07f9386a61
anqi <74876670+anqixxx@users.noreply.github.com>
2022-07-06 14:32:02 -0700

    Create treasure.cpp

---
+++ treasure.cpp
@@ -0,0 +1,4 @@
+// Treasure detecting functions, from ultrasonic sensors, hall effect sensors, and reflectance sensors
+#include "treasure.h"
+
+// Now define the main code for the functions listed in the header file
