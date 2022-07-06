53150bd2d3e8e73b6d7c5dc6b9e936693e3efeb1
anqi <74876670+anqixxx@users.noreply.github.com>
2022-07-06 14:24:50 -0700

    Create field_nav.h

---
+++ field_nav.h
@@ -0,0 +1,10 @@
+// Tape following functions, using reflectance sensors to reorient ourselves
+
+#ifndef FIELD_NAV_H
+#define FIELD_NAV_H
+
+#include <Arduino.h>
+
+//Enter in function definitions for field navigation, with the code located in field_nav.cpp
+
+#endif
