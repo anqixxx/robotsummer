b6b7955430c25fdd85ea06518a1541d1a7208c16
anqi <74876670+anqixxx@users.noreply.github.com>
2022-07-06 14:30:52 -0700

    Create claw.cpp

---
+++ claw.cpp
@@ -0,0 +1,4 @@
+// Claw Functions, reacting to sensors, placement of the claw, speed of the claw, location of the claw
+#include "claw.h"
+
+// Now define the main code for the functions listed in the header file
