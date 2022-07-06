60587e1fc276aea84f9bd84b2abf38a2a12bdaa8
anqi <74876670+anqixxx@users.noreply.github.com>
2022-07-06 14:08:42 -0700

    Create tape_follow.h
    Tape following functions, using reflectance sensors to reorient ourselves

---
+++ tape_follow.h
@@ -0,0 +1,10 @@
+// Tape following functions, using reflectance sensors to reorient ourselves
+
+#ifndef TAPE_FOLLOW_H
+#define TAPE_FOLLOW_H
+
+#include <Arduino.h>
+
+//Enter in function definitions for tape following, with the code located in tape_follow.cpp
+
+#endif
