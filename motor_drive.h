fe30a90e6699ad709bf168ae40c334b62b81c03c
anqi <74876670+anqixxx@users.noreply.github.com>
2022-07-06 14:22:51 -0700

    Created Motor Drive Functions Header File
    Motor Drive Functions, rotary encoder and wheel motor directions

---
+++ motor_drive.h
@@ -0,0 +1,10 @@
+// Motor Drive Functions, rotary encoder and wheel motor directions
+
+#ifndef MOTOR_DRIVE_H
+#define MOTOR_DRIVE_H
+
+#include <Arduino.h>
+
+//Enter in function definitions for motor drive functions, with the code located in motor_drive.cpp
+
+#endif
