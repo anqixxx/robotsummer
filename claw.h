d601d3176ae1da3f8ad4d574a6a7b85fbe5cc224
anqi <74876670+anqixxx@users.noreply.github.com>
2022-07-06 14:25:51 -0700

    Create claw.h

---
+++ claw.h
@@ -0,0 +1,9 @@
+// Claw Functions, reacting to sensors, placement of the claw, speed of the claw, location of the claw
+#ifndef CLAW_H
+#define CLAW_H
+
+#include <Arduino.h>
+
+//Enter in function definitions for claw movement, with the code located in claw.cpp
+
+#endif
