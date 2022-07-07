2df79df817373fde37990874fe30022430b98151
anqi <anqix2002@gmail.com>
2022-07-06 17:34:25 -0700

    added ir sensor code to ir sensor cpp for organization

--- include/ir_sensors.h
+++ include/ir_sensors.h
@@ -4,5 +4,8 @@
 #include <Arduino.h>
 
 //Enter in function definitions for ir sensing, with the code located in ir_sensors.cpp
+void updateReference(int *s, int *c, int cyclePosition);
+
+double takeSquareSignalSample( byte pin, int numReadings, int sampleInterval);
 
 #endif
