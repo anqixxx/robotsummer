9d1e61c109a99ae9d5628f007c91557eb1a0809a
sghyseli <sghyseli@student.ubc.ca>
2022-07-13 18:55:28 -0700

    Changes to RC driving and toggle

--- src/motor_drive.cpp
+++ src/motor_drive.cpp
@@ -10,7 +10,6 @@ DuePWM pwm( PWM_FREQ1, PWM_FREQ2 );
 
 // test speed and drive with motor
 void drive(int speedLeft, int speedRight) {
-      Serial.println("DEBUG");
     //Take speedLeft as Motor 1, speedRight as Motor 2
     if (speedLeft >= 0) {
         analogWrite(L_MOTOR_FORWARD, speedLeft);
