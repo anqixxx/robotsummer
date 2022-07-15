54863fed5220a230bc88bd31cc51a018fb382855
sghyseli <sghyseli@student.ubc.ca>
2022-07-15 11:58:40 -0700

    Staging for PWM debugging

--- src/main.cpp
+++ src/main.cpp
@@ -18,31 +18,47 @@
 #include "RF24.h"
 #include "RF24_config.h"
 
+
 double sigAmp = 0;
 
+int pot1;
+int pot2;
+int tapeLeft;
+int tapeRight;
+
 void manualMode();
+
+
 
 void setup() 
 {
     Serial.begin(SERIAL_RATE);
     Serial.print("Hello");
     blueStart();
-    pwm_setup();
-    setupRadio();
+   pwm_setup();
+  //  setupRadio();  Radio out of service
 }
 
 void loop() 
 { 
+  // RADIO OUT OF SERVICE
+  // rcloop();
+  // if (isManual()){
+  //    Serial.println("in manual mode");
+  //    manualMode();
+  // } else {
+  //    Serial.println("in auto mode");
+  //     drive(0  , 0);
+  //    // lineFollow(potentiometerData()); // poten is from 0 to 255
+  // }
+  pot1 = analogRead(POT1);
+  pot1 = analogRead(POT2);
+  tapeLeft = analogRead(TAPE_L);
+  tapeRight = analogRead(TAPE_R);
 
-  rcloop();
-  //pwm_loop(); // Can this be romoved or archived for if we need DuePWM on pins 6789
-  if (isManual()){
-     Serial.println("in manual mode");
-     manualMode();
-  } else {
-     Serial.println("in auto mode");
-     // lineFollow(potentiometerData()); // poten is from 0 to 255
-  }
+    blueLoop(pot1, pot2, tapeLeft, tapeRight ,0);
+      drive( analogRead(POT1)/10 , analogRead(POT2)/10);
+      
 
 }
 
@@ -50,6 +66,6 @@ void loop()
 void manualMode(){
   int mainLeftMotor = leftMotorData();
   int mainRightMotor = rightMotorData();
-  drive(10  , 10);
+  drive(mainLeftMotor  , mainRightMotor);
   blueLoop(mainLeftMotor, mainRightMotor, 0,  0,  0);
 }
