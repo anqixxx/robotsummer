bcbfc9ec3b035db3ad75cc8f116dde751382f0f9
anqi <anqix2002@gmail.com>
2022-07-28 19:22:24 -0700

    sonar code

--- src/treasure.cpp
+++ src/treasure.cpp
@@ -23,14 +23,29 @@
     Public Domain
 */
 
-// Pins
-const int TRIG_PIN = 10;
-const int ECHO_PIN = 11;
-
 // Anything over 400 cm (23200 us pulse) is "out of range"
 const unsigned int MAX_DIST = 23200;
 
 void ultra_setup() {
+
+  // The Trigger pin will tell the sensor to range find
+  pinMode(RIGHT_TRIG_PIN, OUTPUT);
+  digitalWrite(RIGHT_TRIG_PIN, LOW);
+  pinMode(LEFT_TRIG_PIN, OUTPUT);
+  digitalWrite(LEFT_TRIG_PIN, LOW);
+
+  //Set Echo pin as input to measure the duration of 
+  //pulses coming back from the distance sensor
+  pinMode(RIGHT_ECHO_PIN, INPUT);
+  pinMode(LEFT_ECHO_PIN, INPUT);
+
+
+  // We'll use the serial monitor to view the sensor output
+  Serial.begin(9600);
+}
+
+//Testing Setup
+void test_ultra_setup(int TRIG_PIN, int ECHO_PIN) {
 
   // The Trigger pin will tell the sensor to range find
   pinMode(TRIG_PIN, OUTPUT);
@@ -44,7 +59,8 @@ void ultra_setup() {
   Serial.begin(9600);
 }
 
-void ultra_loop() {
+//Testing Function
+void test_ultra_loop(int TRIG_PIN, int ECHO_PIN) {
 
   unsigned long t1;
   unsigned long t2;
@@ -81,6 +97,79 @@ void ultra_loop() {
     Serial.print(" cm \t");
     Serial.print(inches);
     Serial.println(" in");
+  }
+
+  // Wait at least 60ms before next measurement
+  delay(60);
+}
+
+int sonar_cm(int TRIGGER, int ECHO) {
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float cm;
+
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER, LOW);
+
+  // Wait for pulse on echo pin
+  while ( digitalRead(ECHO) == 0 );
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while ( digitalRead(ECHO) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+  // Calculate distance
+  cm = pulse_width / 58.0;
+
+  // Print out results
+  if ( pulse_width > MAX_DIST ) {
+    Serial.println("Out of range");
+    return -1;
+  } else {
+    return cm;
+  }
+
+  // Wait at least 60ms before next measurement
+  delay(60);
+}
+
+int sonar_in(int TRIGGER, int ECHO) {
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long pulse_width;
+  float inches;
+
+  // Hold the trigger pin high for at least 10 us
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER, LOW);
+
+  // Wait for pulse on echo pin
+  while ( digitalRead(ECHO) == 0 );
+
+  // Measure how long the echo pin was held high (pulse width)
+  // Note: the micros() counter will overflow after ~70 min
+  t1 = micros();
+  while ( digitalRead(ECHO) == 1);
+  t2 = micros();
+  pulse_width = t2 - t1;
+
+  // Calculate distance
+  inches = pulse_width / 148.0;
+
+  // Print out results
+  if ( pulse_width > MAX_DIST ) {
+    Serial.println("Out of range");
+    return -1;
+    //shows that it is out of range
+  } else {
+    return inches;
   }
 
   // Wait at least 60ms before next measurement
